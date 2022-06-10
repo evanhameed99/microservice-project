4c7195a1e5dbb63e1f5111f68e813f5aa84fd1f0
Evan Hameed <63654749+evanhameed99@users.noreply.github.com>
2022-06-10 18:51:42 +0300

    set envs for api gateway

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
-
+import * as Joi from 'joi';
+import { DatabaseModule } from 'libs/common';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/gateway/src/.env',
+    }),
+    DatabaseModule,
+  ],
   controllers: [ApiGatewayController],
   providers: [ApiGatewayService],
 })
